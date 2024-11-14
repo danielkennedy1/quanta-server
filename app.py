73e4b791148b250e989a64dab619c02de72debad
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-11-14 18:11:27 +0000

    Root logger config

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from connexion.resolver import RelativeResolver
 from config.config import Config
 import logging
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 config = Config("config/config.json")
 config.configure_logger(logger)
@@ -11,8 +11,3 @@ config.configure_logger(logger)
 app = App(__name__)
 
 app.add_api('api.yaml', resolver=RelativeResolver("adapters.controller.controller"))
-
-#config.configure_logger(logging.getLogger("uvicorn.access"))
-#config.configure_logger(logging.getLogger("uvicorn.error"))
-
-logger.error("\n".join(logging.root.manager.loggerDict.keys()))
