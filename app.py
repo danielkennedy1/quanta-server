f07adfbd031d61496afd90ab3cb956dde97b3c7d
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-11-14 18:03:49 +0000

    Messing with loggers

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ logger = logging.getLogger(__name__)
 config = Config("config/config.json")
 config.configure_logger(logger)
 
-uvicorn_logger = logging.getLogger("uvicorn")
-config.configure_logger(uvicorn_logger)
-
 app = App(__name__)
 
 app.add_api('api.yaml', resolver=RelativeResolver("adapters.controller.controller"))
+
+#config.configure_logger(logging.getLogger("uvicorn.access"))
+#config.configure_logger(logging.getLogger("uvicorn.error"))
+
+logger.error("\n".join(logging.root.manager.loggerDict.keys()))
